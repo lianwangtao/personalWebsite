18802ee8ed04296c367b40513bc1a459111be390
lianwangtao <wlian3@wisc.edu>
2016-03-23 15:59:32 -0500

    added hover effect and smooth scroll

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
  */
 
  'use strict';
-var app = angular.module('shane', []);
+var app = angular.module('shane', ['duScroll']);
 
 app.controller('projectsListCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('projects.json').success(function(data) {
@@ -13,11 +13,16 @@ app.controller('projectsListCtrl', ['$scope', '$http', function($scope, $http) {
 
 app.controller('windowsizeCtrl', ['$scope', '$window', function($scope, $window) {
   var width = document.getElementById("main").clientWidth;
-  if (width > 1265) {
-    $scope.size = "col-md-offset-3";
-  } else {
-    $scope.size = "col-md-offset-2";
+  if (width < 768) {
+    $scope.size = "brow col-xs-offset-1";
+  } else if (width >= 768 && width < 992) {
+    $scope.size = "brow col-sm-offset-1";
+  } else if (width >= 992 && width <= 1265) {
+    $scope.size = "brow col-md-offset-2";
+  } else if (width > 1265) {
+    $scope.size = "brow col-lg-9 col-lg-offset-3";
   }
+
 
   $(".brick1").hover(function() {
     $(this).addClass("pulse");
@@ -26,80 +31,3 @@ app.controller('windowsizeCtrl', ['$scope', '$window', function($scope, $window)
   });
 
 }]);
-
-
-// Scrolling var app = angular.module('app', []);
-
-app.service('anchorSmoothScroll', function(){
-
-    this.scrollTo = function(eID) {
-
-        // This scrolling function
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
-
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY); return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 30) speed = 30;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for ( var i=startY; i<stopY; i+=step ) {
-                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
-        }
-        for ( var i=startY; i>stopY; i-=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-        }
-
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
-
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
-
-    };
-
-});
-
-app.controller('ScrollCtrl', function($scope, $location, anchorSmoothScroll) {
-
-    $scope.gotoElement = function (eID){
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('main');
-      $location.hash('aboutme');
-      $location.hash('projects');
-      $location.hash('resume');
-      $location.hash('contact');
-
-      // call $anchorScroll()
-      anchorSmoothScroll.scrollTo(eID);
-
-    };
-
-  });
-
-// End of Scrolling
