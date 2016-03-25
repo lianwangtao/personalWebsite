437333205ef6d9fc7685142dd27ef176914124e9
lianwangtao <wlian3@wisc.edu>
2016-03-25 03:56:04 -0500

    before sending

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,54 @@ app.controller('projectsListCtrl', ['$scope', '$http', function($scope, $http) {
   });
 }]);
 
+
+app.controller('expandCtrl', ['$scope', '$window', function($scope, $window) {
+  var i = 0;
+  (function() {
+          function SVGExpander( el, options ) {
+            this.el = el;
+            this.init();
+          }
+          SVGExpander.prototype.init = function() {
+            this.trigger = this.el.querySelector( 'button.trigger' );
+            this.shapeEl = this.el.querySelector( 'span.morph-shape' );
+            var s = Snap( this.shapeEl.querySelector( 'svg' ) );
+            this.pathEl = s.select( 'path' );
+            this.paths = {
+              reset : this.pathEl.attr( 'd' ),
+              open : this.shapeEl.getAttribute( 'data-morph-open' ),
+              close : this.shapeEl.getAttribute( 'data-morph-close' )
+            };
+            this.isOpen = false;
+            this.initEvents();
+          };
+          SVGExpander.prototype.initEvents = function() {
+            this.trigger.addEventListener( 'click', this.toggle.bind(this) );
+          };
+          SVGExpander.prototype.toggle = function() {
+            var self = this;
+            if( this.isOpen ) {
+              this.pathEl.stop().animate( { 'path' : this.paths.close }, 250, mina.easeout, function() {
+                self.pathEl.stop().animate( { 'path' : self.paths.reset }, 800, mina.elastic );
+              } );
+              setTimeout( function() { classie.remove( self.el, 'box--sizeup' ); }, 250 );
+            }
+            else {
+              this.pathEl.stop().animate( { 'path' : this.paths.open }, 250, mina.easeout, function() {
+                self.pathEl.stop().animate( { 'path' : self.paths.reset }, 800, mina.elastic );
+              } );
+              setTimeout( function() { classie.add( self.el, 'box--sizeup' ); }, 250 );
+            }
+            this.isOpen = !this.isOpen;
+            i++;
+            console.log("now the i is " + i);
+          };
+          [].slice.call( document.querySelectorAll( '.box--collapser' ) ).forEach( function( el ) { new SVGCollapser(el); } );
+          new SVGExpander( document.getElementById( 'expander' ) );
+        })();
+}]);
+
+
 app.controller('windowsizeCtrl', ['$scope', '$window', function($scope, $window) {
   var width = document.getElementById("main").clientWidth;
   if (width < 768) {
